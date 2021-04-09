f3e1eaa2061d15523bb835feb2f72b79dd5de82e
sahil.jayswal <sahil.jayswal1995@gmail.com>
2021-04-09 18:12:12 +0530

    Starting development of LockedMe.Com Application, Added Application and Developer Detail

---
+++ src/LockedMe.java
@@ -0,0 +1,18 @@
+
+public class LockedMe {
+	
+	
+
+	public static void main(String[] args) {
+		
+		final String Welcome_Message = "******************** LockedMe.Com ********************" +
+										"\n******************** SAHIL JAYSWAL ********************";	
+		System.out.println(Welcome_Message);
+		
+//		LockedMeOption option = new LockedMeOption();
+//		
+		
+	
+	}
+
+}
