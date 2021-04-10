9cab4444dbd6156223b5ec971b08d57e268a522a
sahil.jayswal <sahil.jayswal1995@gmail.com>
2021-04-10 16:06:44 +0530

    Added all Option Operations

--- src/LockedMe.java
+++ src/LockedMe.java
@@ -1,12 +1,10 @@
-
 public class LockedMe {
-	
-	
 
-	public static void main(String[] args) {
+
+	public static void main(String[] args){
 		
-		final String Welcome_Message = "******************** LockedMe.Com ********************" +
-										"\n******************** SAHIL JAYSWAL ********************";	
+		final String Welcome_Message = "******************** Welcome to LockedMe.Com ********************" +
+										"\n******************** Developed by SAHIL JAYSWAL ********************";	
 		System.out.println(Welcome_Message);
 		
 		LockedMeOption option = new LockedMeOption();
@@ -14,5 +12,4 @@ public class LockedMe {
 		
 	
 	}
-
 }
--- src/LockedMeOption.java
+++ src/LockedMeOption.java
@@ -1,18 +1,30 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class LockedMeOption {
+	
+	//Fetching directory properties
+	String Directory = System.getProperty("user.dir");
+	File folder_name = new File(Directory);
+	
+	//Primary Option for user
 	private static final String Primary_Option =
             "\nSelect any of the following: \n"+
                     "1 -> List files in directory\n"+
                     "2 -> Add, Delete or Search\n"+
                     "3 -> Exit Program";
 
+	//Secondry Option for user
     private static final String Secondary_Option =
             "   \nSelect any of the following: \n"+
                     "   1 -> Add a file\n"+
                     "   2 -> Delete a file\n"+
                     "   3 -> Search a file\n"+
                     "   4 -> GoBack";
+    
+    //Primary Option UserInput and Operations
     void PrimaryOption() {
     	System.out.println(Primary_Option);
     	@SuppressWarnings("resource")
@@ -20,7 +32,8 @@ public class LockedMeOption {
     	int PriInput = sc.nextInt();
     	switch(PriInput) {
     	case 1:
-    		
+    		ListFile();
+    		PrimaryOption();    		
     		break;
     		
     	case 2:
@@ -28,7 +41,8 @@ public class LockedMeOption {
     		break;
     		
     	case 3:
-    		
+    		System.out.println("Exiting Application..." + "\nThank You!");
+    		System.exit(0);
     		 break;
     		 
     	default:
@@ -38,22 +52,30 @@ public class LockedMeOption {
     	
     }
     
+    //Secondary Option UserInput and Operation
     void SecondaryOption() {
     	System.out.println(Secondary_Option);
+    	try {
     	@SuppressWarnings("resource")
 		Scanner sc = new Scanner(System.in);
     	int SecInput = sc.nextInt();
     	switch(SecInput) {
     	case 1:
-    		
+    		System.out.println("Please enter file name to Add: ");
+    		String filename = sc.next();
+    		AddFile(filename);
     		break;
     		
     	case 2:
-    		
+    		System.out.println("Please enter file name to delete: ");
+    	    filename = sc.next();
+    	    DeleteFile(filename);
     		break;
     		
     	case 3:
-    		
+    		System.out.println("Please enter file name to search: ");
+    		filename = sc.next();
+    		SearchFile(filename);
     		break;
     		
     	case 4:
@@ -65,8 +87,71 @@ public class LockedMeOption {
     		System.out.println("Please enter 1, 2, 3 or 4");
     		SecondaryOption();
     	}
+    	//Call secondary options
+    	SecondaryOption();
+    	}
     	
-    	
+    	catch(IOException e) {
+    		System.out.println("Please select option 1, 2 or 3");
+    		PrimaryOption();
+    	}
     }
-
+    
+    //List files Operation
+    void ListFile() {
+    	String[] list;
+    	File f = new File(Directory);
+    	if(f.list().length==0) {
+    		System.out.println("The Folder is empty");
+    	}
+    	else {
+    		list = f.list();
+    		Arrays.sort(list);
+    		System.out.println("The File in " + f + " are: ");
+    		for(String str:list) {
+    			System.out.println(str);
+    		}
+    		
+    	}
+    }
+    
+    //Adding file provided by user
+    void AddFile(String filename) throws IOException {
+    	File f = new File(folder_name+"/"+filename);
+    	String[] list = folder_name.list();
+    	for(String str:list) {
+    		if(filename.equalsIgnoreCase(str)) {
+    			System.out.println("File " + filename + " is already exist at " +folder_name);
+    			return;
+    		}
+    	}
+    	f.createNewFile();
+    	System.out.println("File " + filename + " is craeted successfully in " +folder_name);  	
+    
+    }
+    
+    //Deleting file selected by user
+    void DeleteFile(String filename) {
+    	File f = new File(folder_name+"/"+filename);
+    	String[] list = folder_name.list();
+    	for(String str:list) {
+    		if(filename.equals(str) && f.delete()) {
+    			System.out.println("File " + filename + " deleted successfully from " + folder_name);
+    			return;
+    		}
+    	}
+    	System.out.println("Delete failed file not found");
+    }
+    //Search File3
+    void SearchFile(String filename) {
+    	String[] list = folder_name.list();
+    	for(String str:list) {
+    		if(filename.equals(str)) {
+    			System.out.println("File " + filename + " found at: " + folder_name);
+    			return;
+    		}
+    	}
+    	System.out.println("File not found");
+    }
 }
+
